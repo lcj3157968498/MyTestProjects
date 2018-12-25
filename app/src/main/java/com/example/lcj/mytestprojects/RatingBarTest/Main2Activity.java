e54d0d1b77a4b014c0a452bcfb551750cb754210
e <Lcj@163.com>
2018-12-25 21:22:39 +0800

    22

--- app/src/main/java/com/example/lcj/mytestprojects/RatingBarTest/Main2Activity.java
+++ app/src/main/java/com/example/lcj/mytestprojects/RatingBarTest/Main2Activity.java
@@ -56,7 +56,7 @@ public class Main2Activity extends AppCompatActivity {
                         mSelectTags.add(name);
                     }
                 }
-                System.out.println("1111");
+                System.out.println("222");
                 //Toast.makeText(Main2Activity.this, mSelectTags.toString(), Toast.LENGTH_SHORT).show();
 
                 // Toast.makeText(Main2Activity.this,mTagsAdapter.mToastList.toString(),Toast.LENGTH_SHORT).show();
